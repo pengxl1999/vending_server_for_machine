df8b75bcbe7c38ebea671335acd7c58fc2c05e83
pengxl <pengxl1999@126.com>
2019-07-13 21:08:10 +0800

    query result

--- web/notify.php
+++ web/notify.php
@@ -13,20 +13,18 @@ if(!$result) {
 if($result) {
 //    $out_trade_no = $_POST['out_trade_no'];
 //    $trade_status = $_POST['trade_status'];
-
-
-    $customerPurchase = \app\models\CustomerPurchase::findOne(['cp_order' => $arr['out_trade_no']]);
-    if($arr['trade_status'] == 'WAIT_BUYER_PAY') {
-        $customerPurchase->status = 1;
-        $customerPurchase->save();
-    }
-    else if($arr['trade_status'] == 'TRADE_SUCCESS') {
-        $customerPurchase->status = 2;
-        $customerPurchase->save();
-    }
-    //$alipayService->writeLog($arr);
     $alipayService->writeLog($arr['out_trade_no']);
     $alipayService->writeLog($arr['trade_status']);
+    $customerPurchase = \app\models\CustomerPurchase::findOne(['cp_order' => $arr['out_trade_no']]);
+//    if($arr['trade_status'] == 'WAIT_BUYER_PAY') {
+//        $customerPurchase->status = 1;
+//        $customerPurchase->save();
+//    }
+//    else if($arr['trade_status'] == 'TRADE_SUCCESS') {
+//        $customerPurchase->status = 2;
+//        $customerPurchase->save();
+//    }
+    //$alipayService->writeLog($arr);
     echo 'success';
 }
 else {
