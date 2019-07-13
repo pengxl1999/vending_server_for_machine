21b65e445390782876c76a54d3bee63fa9eca8f1
pengxl <pengxl1999@126.com>
2019-07-13 20:53:10 +0800

    query result

--- web/notify.php
+++ web/notify.php
@@ -8,17 +8,17 @@ $alipayService->writeLog(var_export($_POST, true));
 $result = $alipayService->check($arr);
 
 if($result) {
-    $out_trade_no = $_POST['out_trade_no'];
-    $trade_status = $_POST['trade_status'];
+//    $out_trade_no = $_POST['out_trade_no'];
+//    $trade_status = $_POST['trade_status'];
     $alipayService->writeLog($arr);
-    $alipayService->writeLog($out_trade_no);
-    $alipayService->writeLog($trade_status);
-    $customerPurchase = \app\models\CustomerPurchase::findOne(['cp_order' => $out_trade_no]);
-    if($trade_status == 'WAIT_BUYER_PAY') {
+    $alipayService->writeLog($_POST['out_trade_no']);
+    $alipayService->writeLog($_POST['trade_status']);
+    $customerPurchase = \app\models\CustomerPurchase::findOne(['cp_order' => $_POST['out_trade_no']]);
+    if($_POST['trade_status'] == 'WAIT_BUYER_PAY') {
         $customerPurchase->status = 1;
         $customerPurchase->save();
     }
-    else if($trade_status == 'TRADE_SUCCESS') {
+    else if($_POST['trade_status'] == 'TRADE_SUCCESS') {
         $customerPurchase->status = 2;
         $customerPurchase->save();
     }
