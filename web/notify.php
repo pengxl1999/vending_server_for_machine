d83970a9879aeba8739cfb78544a907c7e599cd1
pengxl <pengxl1999@126.com>
2019-07-13 20:54:44 +0800

    query result

--- web/notify.php
+++ web/notify.php
@@ -11,14 +11,14 @@ if($result) {
 //    $out_trade_no = $_POST['out_trade_no'];
 //    $trade_status = $_POST['trade_status'];
     $alipayService->writeLog($arr);
-    $alipayService->writeLog($_POST['out_trade_no']);
-    $alipayService->writeLog($_POST['trade_status']);
-    $customerPurchase = \app\models\CustomerPurchase::findOne(['cp_order' => $_POST['out_trade_no']]);
-    if($_POST['trade_status'] == 'WAIT_BUYER_PAY') {
+    $alipayService->writeLog($arr['out_trade_no']);
+    $alipayService->writeLog($arr['trade_status']);
+    $customerPurchase = \app\models\CustomerPurchase::findOne(['cp_order' => $arr['out_trade_no']]);
+    if($arr['trade_status'] == 'WAIT_BUYER_PAY') {
         $customerPurchase->status = 1;
         $customerPurchase->save();
     }
-    else if($_POST['trade_status'] == 'TRADE_SUCCESS') {
+    else if($arr['trade_status'] == 'TRADE_SUCCESS') {
         $customerPurchase->status = 2;
         $customerPurchase->save();
     }
