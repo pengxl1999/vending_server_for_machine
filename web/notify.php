42ddde76aa84e0060a2535947f2b60c9a9da827d
pengxl <pengxl1999@126.com>
2019-07-13 21:38:03 +0800

    query result

--- web/notify.php
+++ web/notify.php
@@ -1,10 +1,10 @@
 <?php
-require_once '../models/CustomerPurchase.php';
 require_once '../vendor/alipay/f2fpay/service/AlipayTradeService.php';
 require_once '../config/params.php';
+require '../models/CustomerPurchase.php';
 
 $arr = $_POST;
-$alipayService = new \AlipayTradeService($alipay);
+$alipayService = new AlipayTradeService($alipay);
 $alipayService->writeLog(var_export($arr, true));
 $result = $alipayService->check($arr);
 
@@ -14,18 +14,19 @@ if(!$result) {
 }
 else {
     $out_trade_no = $arr['out_trade_no'];
-    $customerPurchase = \app\models\CustomerPurchase::findOne(['cp_order' => $out_trade_no]);
-    if($customerPurchase == null) {
-        $alipayService->writeLog("hehe");
-    }
-    $alipayService->writeLog($customerPurchase->cp_order);
-    if($arr['trade_status'] == 'WAIT_BUYER_PAY') {
-        $customerPurchase->status = 1;
-        $customerPurchase->save();
-    }
-    else if($arr['trade_status'] == 'TRADE_SUCCESS') {
-        $customerPurchase->status = 2;
-        $customerPurchase->save();
-    }
+    //$customerPurchase = \app\models\CustomerPurchase::findOne(['cp_order' => $out_trade_no]);
+//    if($customerPurchase == null) {
+//        $alipayService->writeLog("hehe");
+//    }
+    $alipayService->writeLog("haha");
+//    $alipayService->writeLog($customerPurchase->cp_order);
+//    if($arr['trade_status'] == 'WAIT_BUYER_PAY') {
+//        $customerPurchase->status = 1;
+//        $customerPurchase->save();
+//    }
+//    else if($arr['trade_status'] == 'TRADE_SUCCESS') {
+//        $customerPurchase->status = 2;
+//        $customerPurchase->save();
+//    }
     echo 'success';
 }
