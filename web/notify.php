04c83c1a17933c699cea54cf13996064393f23c5
pengxl <pengxl1999@126.com>
2019-07-14 20:45:30 +0800

    query result

--- web/notify.php
+++ web/notify.php
@@ -14,20 +14,20 @@ if (!$result) {
 //    if($customerPurchase == null) {
 //        $alipayService->writeLog("hehe");
 //    }
-    $db = new mysqli('127.0.0.1:3306', 'root', 'root', 'vending');
-    if($db->connect_errno) {
-        $alipayService->writeLog("Database Error!");
-        die("could not connect to the database:\n" . $db->connect_error);
-    }
-    $db->query("set names 'utf8';");
-    $sql = "UPDATE customer_purchase SET status = '1' WHERE cp_order = '". $out_trade_no . "'";
-    $res = $db->query($sql);
-    if (!$res) {
-        $alipayService->writeLog("Query Error!");
-        die("sql error:\n" . $db->error);
-    }
-    $res->free();
-    $db->close();
+//    $db = new mysqli('127.0.0.1:3306', 'root', 'root', 'vending');
+//    if($db->connect_errno) {
+//        $alipayService->writeLog("Database Error!");
+//        die("could not connect to the database:\n" . $db->connect_error);
+//    }
+//    $db->query("set names 'utf8';");
+//    $sql = "UPDATE customer_purchase SET status = '1' WHERE cp_order = '". $out_trade_no . "'";
+//    $res = $db->query($sql);
+//    if (!$res) {
+//        $alipayService->writeLog("Query Error!");
+//        die("sql error:\n" . $db->error);
+//    }
+//    $res->free();
+//    $db->close();
     $alipayService->writeLog("haha");
 //    $alipayService->writeLog($customerPurchase->cp_order);
 //    if($arr['trade_status'] == 'WAIT_BUYER_PAY') {
