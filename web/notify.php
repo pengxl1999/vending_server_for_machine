d57580365525608d2a8d10830d9201129794cefc
pengxl <pengxl1999@126.com>
2019-07-14 19:56:18 +0800

    query result

--- web/notify.php
+++ web/notify.php
@@ -1,25 +1,23 @@
 <?php
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
+require __DIR__ . '/../config/web.php';
 require_once '../vendor/alipay/f2fpay/service/AlipayTradeService.php';
-require_once '../config/params.php';
 
 $arr = $_POST;
 $alipayService = new AlipayTradeService($alipay);
 $alipayService->writeLog(var_export($arr, true));
 $result = $alipayService->check($arr);
 
-if(!$result) {
+if (!$result) {
     $alipayService->writeLog("false");
     echo 'fail';
-}
-else {
+} else {
     $out_trade_no = $arr['out_trade_no'];
-    //$customerPurchase = \app\models\CustomerPurchase::findOne(['cp_order' => $out_trade_no]);
-    $test = \app\models\CustomerPurchase::getMaxId();
 //    if($customerPurchase == null) {
 //        $alipayService->writeLog("hehe");
 //    }
     $alipayService->writeLog("haha");
-    $alipayService->writeLog($test);
 //    $alipayService->writeLog($customerPurchase->cp_order);
 //    if($arr['trade_status'] == 'WAIT_BUYER_PAY') {
 //        $customerPurchase->status = 1;
