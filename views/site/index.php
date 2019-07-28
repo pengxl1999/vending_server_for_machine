4acf02bfa8f5da80e37aab7816c6702d2b544c63
pengxl <pengxl1999@126.com>
2019-07-28 08:34:38 +0800

    check

--- views/site/index.php
+++ views/site/index.php
@@ -35,10 +35,7 @@ $this->title = 'Vending Machine';
             <div class="col-lg-4">
                 <h2>关于我们</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>TO Group of Jilin University是吉林大学计算机科学与技术学院无线传感器网络实验室物联网设备小组</p>
 
                 <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
             </div>
