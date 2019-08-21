ca41071f95b53b7bd8c671dc077a85f9ebd90aa6
pengxl <pengxl1999@126.com>
2019-08-21 12:55:16 +0800

    voice input

--- views/buy/index.php
+++ views/buy/index.php
@@ -25,8 +25,8 @@ $this->params['breadcrumbs'][] = $this->title;
     <form id='search_form' action="./index.php?r=buy/index" method="post">
         <input type="text" name="search_med" id='search_med' placeholder="搜索药品" style="font-size: x-large" value=""/>
         <input type="submit" value="搜索" class="btn btn-primary" style="font-size:x-large; margin-left: 15px" />
+        <a class="btn btn-primary" style="font-size:x-large; margin-left: 15px" onclick="window.android.voiceInput()">语音输入</a>
     </form>
-    <a class="btn btn-primary" style="font-size:x-large; margin-left: 15px" onclick="window.android.voiceInput()">语音输入</a>
 
     <br/>
 
