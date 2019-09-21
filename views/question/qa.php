9fd1366c45eb06c3d3430cacbf7dee91dc45f10c
pengxl <pengxl1999@126.com>
2019-09-21 19:31:13 +0800

    健康咨询

--- views/question/qa.php
+++ views/question/qa.php
@@ -2,10 +2,15 @@
     echo 'haveddd';
 ?>
 
+<form id='search_form' action="./index.php?r=buy/index" method="post">
+    <input type="text" name="search_med" id='search_med' placeholder="搜索药品" style="font-size: x-large" value=""/>
+    <input type="submit" value="搜索" class="btn btn-primary" style="font-size:x-large; margin-left: 15px" />
+</form>
+
 <script>
     function haveFun() {
-        document.write("11111");
-        alert("343");
-        window.android.haha();
+        var form = document.getElementById('search_form');
+        var med = document.getElementById('search_med');
+        med.value = "haahh";
     }
 </script>
