dd775daca72fb144a31301b54fd31689ddd5dae6
pengxl <pengxl1999@126.com>
2019-09-23 20:21:32 +0800

    健康咨询

--- views/question/qa.php
+++ views/question/qa.php
@@ -1,7 +1,6 @@
 <form id='search_form' action="./index.php?r=question/qa" method="post">
     <input type="text" name="info_search" id='search_med' placeholder="搜索药品" style="font-size: x-large" value=""/>
     <input type="submit" value="搜索" class="btn btn-primary" style="font-size:x-large; margin-left: 15px" />
-    <a class="btn btn-primary" style="font-size:x-large; margin-left: 15px" onclick="window.android.voiceInput()">语音输入</a>
 </form>
 
 <script type="text/javascript">
@@ -25,6 +24,6 @@
 /* @var $voice */
 /* @var $flag */
 if($flag) {
-    echo "<script type=text/javascript>haveFun('". $result . "', '" . $voice ." ')</script>";
+    //echo "<script type=text/javascript>haveFun('". $result . "', '" . $voice ." ')</script>";
 }
 ?>
