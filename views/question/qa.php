5156fa0776add1c85b7cb8de1d0ce0f53abb0019
pengxl <pengxl1999@126.com>
2019-09-23 17:52:35 +0800

    健康咨询

--- views/question/qa.php
+++ views/question/qa.php
@@ -6,19 +6,10 @@
         echo "<script type=text/javascript>haveFun('" . $result ."', '" . $voice . "')</script>";
 ?>
 
-<form id="info_form" action="./index.php?r=question/qa" method="post">
-    <input type="text" name="info_search" id='search_med' placeholder="搜索药品" style="font-size: x-large"/>
-    <input type="submit" value="搜索" class="btn btn-primary" style="font-size:x-large; margin-left: 15px" />
-</form>
-
 
 <script>
     function getInformation(arg) {
-        window.android.haha();
-        var form = document.getElementById('info_form');
-        var input = document.getElementById('info_search');
-        input.value = arg;
-        form.submit();
+        window.android.haha(arg);
     }
 
     function haveFun(arg1, arg2) {
